f9589ab090357718ccb044f208becbfbd06747d7
jpaquim <joaopaquim@gmail.com>
2022-12-27 14:46:14 +0000

    Use @sveltejs/adapter-static, remove sverdle

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const auto =
+	!!process.env.VERCEL ||
+	!!process.env.CF_PAGES ||
+	!!process.env.NETLIFY ||
+	process.env.GITHUB_ACTION_REPOSITORY === 'Azure/static-web-apps-deploy';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +15,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: auto ? adapterAuto() : adapterStatic()
 	}
 };
 
